a3e2f0aed1091f2c3ad917b16e7c14f995963bff
arnoldvanandel <ava@fmid.nl>
2020-02-09 16:35:54 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install python3-pip
+sudo pip3 install flask
+sudo pip3 install Flask-SQLAlchemy
+sudo apt-get install g++
+sudo apt-get install unixodbc-dev
+sudo pip3 install pyodbc
+sudo pip3 install pandas
